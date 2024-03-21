fa611104817f3d285c54d786e892626a796e9334
Peter <inbox.peterw@gmail.com>
2024-03-21 20:51:07 +1000

    helper comments

--- src/helpers.ts
+++ src/helpers.ts
@@ -10,14 +10,24 @@ const allowedCommands = new Set(['L', 'R', 'M']);
  */
 export const allowedOrientations = new Set(['N', 'S', 'E', 'W']);
 
+/**
+ * Returns string without spaces.
+ */
 export const removeSpaces = (str: string) => {
 	return str.replace(/\s/g, '');
 };
 
+/**
+ * Checks number type and value 0 or above.
+ */
 export const isValidInteger = (input: number): boolean => {
 	return !isNaN(input) && input >= 0;
 };
 
+/**
+ * Presents prompt to user via readline instance.
+ * Returns a promise that resolves to the user input.
+ */
 export const ask = (
 	rl: readlineModule.Interface,
 	prompt: string
@@ -29,6 +39,9 @@ export const ask = (
 	});
 };
 
+/**
+ * Checks that every command in the input array is allowed.
+ */
 export const isValidCommandSet = (commands: string[]): boolean => {
 	for (const command of commands) {
 		if (!allowedCommands.has(command)) return false;
